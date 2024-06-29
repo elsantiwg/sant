39c44da48057386bfb52ada73c34c2e8063c7122
elsantiwg <122948040+elsantiwg@users.noreply.github.com>
2024-06-29 00:17:22 -0500

    end points

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
-// config.js
-export const PORT = process.env.PORT ?? 3000
+export const {
+  PORT = 3000
+} = process.env
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import express from 'express'
-import { PORT } from './config'
+import { PORT } from './config.js'
 
 const app = express()
 
-app.get('/',(req, res)=>{
-    res.send('hello world')
+app.get('/', (req, res) => {
+  res.send('hello world')
 })
 
-app.listen(PORT, ()=>{
-    console.log('Server running on port ${PORT}')
+app.post('/login', (req, res) => {})
+app.post('/register', (req, res) => {})
+app.post('/logout', (req, res) => {})
+app.post('/protected', (req, res) => {})
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-template-curly-in-string
+  console.log('Server running on port ${PORT}')
 })
